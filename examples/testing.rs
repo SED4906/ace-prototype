e9a2f7d6a39a79d80999a77a62cd930c29a0fd32
SED4906 <sed4906birdie@gmail.com>
2024-08-28 16:02:12 -0700

    rust formatting

--- examples/testing.rs
+++ examples/testing.rs
@@ -1,20 +1,74 @@
-use chrono::Utc;
 use ace_prototype::object;
 use ace_prototype::object::Fundamental;
+use chrono::Utc;
 
 fn main() {
     {
-	let a = {
-	    let mut universe = object::UNIVERSE.lock().unwrap();
-	    universe.insert("A".into(), object::Object::promote(&object::PureObject::new("A".into(), object::Data::Bytes(vec![0x41]))));
-	    universe.insert("B".into(), object::Object::promote(&object::PureObject::new("B".into(), object::Data::Bytes(vec![0x42]))));
-	    universe.insert("C".into(), object::Object::promote(&object::PureObject::new("C".into(), object::Data::Bytes(vec![0x43]))));
-	    universe.get_mut("A").unwrap().compose(object::Composition::new(object::PureObject::new("".into(), object::Data::Reference("Concatenate".into())), vec![object::PureObject::new("".into(), object::Data::Reference("B".into())),object::PureObject::new("".into(), object::Data::Reference("C".into()))]));
-	    universe.get_mut("C").unwrap().compose(object::Composition::new(object::PureObject::new("".into(), object::Data::Reference("Concatenate".into())), vec![object::PureObject::new("".into(), object::Data::Reference("C".into()))]));
-    	    universe.get_mut("A").unwrap().compose(object::Composition::new(object::PureObject::new("".into(), object::Data::Reference("Concatenate".into())), vec![object::PureObject::new("".into(), object::Data::Reference("B".into())),object::PureObject::new("".into(), object::Data::Reference("C".into()))]));
-	    println!("{universe:?}");
-	    universe["A"].clone()
-	};
-	println!("{:?}", a.evaluate(Utc::now()))
+        let a = {
+            let mut universe = object::UNIVERSE.lock().unwrap();
+            universe.insert(
+                "A".into(),
+                object::Object::promote(&object::PureObject::new(
+                    "A".into(),
+                    object::Data::Bytes(vec![0x41]),
+                )),
+            );
+            universe.insert(
+                "B".into(),
+                object::Object::promote(&object::PureObject::new(
+                    "B".into(),
+                    object::Data::Bytes(vec![0x42]),
+                )),
+            );
+            universe.insert(
+                "C".into(),
+                object::Object::promote(&object::PureObject::new(
+                    "C".into(),
+                    object::Data::Bytes(vec![0x43]),
+                )),
+            );
+            universe
+                .get_mut("A")
+                .unwrap()
+                .compose(object::Composition::new(
+                    object::PureObject::new(
+                        "".into(),
+                        object::Data::Reference("Concatenate".into()),
+                    ),
+                    vec![
+                        object::PureObject::new("".into(), object::Data::Reference("B".into())),
+                        object::PureObject::new("".into(), object::Data::Reference("C".into())),
+                    ],
+                ));
+            universe
+                .get_mut("C")
+                .unwrap()
+                .compose(object::Composition::new(
+                    object::PureObject::new(
+                        "".into(),
+                        object::Data::Reference("Concatenate".into()),
+                    ),
+                    vec![object::PureObject::new(
+                        "".into(),
+                        object::Data::Reference("C".into()),
+                    )],
+                ));
+            universe
+                .get_mut("A")
+                .unwrap()
+                .compose(object::Composition::new(
+                    object::PureObject::new(
+                        "".into(),
+                        object::Data::Reference("Concatenate".into()),
+                    ),
+                    vec![
+                        object::PureObject::new("".into(), object::Data::Reference("B".into())),
+                        object::PureObject::new("".into(), object::Data::Reference("C".into())),
+                    ],
+                ));
+            println!("{universe:#?}");
+            universe["A"].clone()
+        };
+        println!("{:?}", a.evaluate(Utc::now()))
     }
 }
